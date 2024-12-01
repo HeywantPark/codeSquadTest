6caff2ad6f636970951536a825a2c677001a4499
Park Hyewon <db1648@naver.com>
2024-12-02 01:55:25 +0900

    소코반 1단계 : 맵 정보를 객체로 저장 및 출력하기 수정

--- src/main/java/org/example/codesquadtest/socoban/SocobanV1.java
+++ src/main/java/org/example/codesquadtest/socoban/SocobanV1.java
@@ -42,14 +42,33 @@ public class SocobanV1 {
 
             String name = line[0]; // 스테이지 이름 (첫줄)
             char[][] map = new char[line.length - 1][];
+            int[][] mappedData = new int[line.length - 1][];
+
 
             // 맵 데이터 저장
             for (int i = 1; i < line.length; i++) {
                 map[i - 1] = line[i].toCharArray();
+                mappedData[i - 1] = convertData(line[i]);
             }
-            stages.add(new Stage(name,map));
+            stages.add(new Stage(name,map,mappedData));
         }
         return stages;
+    }
+
+    private static int[] convertData(String s) {
+        int[] rowData = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            rowData[i] = switch (c) {
+                case ' ' -> 0; // 빈공간
+                case 'O' -> 1; // 구멍
+                case 'o' -> 2; // 공
+                case 'P' -> 3; // 플레이어
+                case '#' -> 4; // 벽
+                default -> 0;  // 기타 (예: 잘못된 값은 빈 공간으로 처리)
+            };
+        }
+        return rowData;
     }
 
 }
@@ -57,15 +76,46 @@ public class SocobanV1 {
 class Stage {
     private final String name; // 스테이지 이름
     private final char[][] map; // 맵 데이터
+    private final int[][] mappedData;  // 변환된 숫자 데이터
 
-    public Stage(String name, char[][] map) {
+    public Stage(String name, char[][] map, int[][] mappedData) {
         this.name = name;
         this.map = map;
+        this.mappedData = mappedData;
     }
+    // 출력하는 메서드
     public void printStage() {
+        // 스테이지 이름 출력
         System.out.println(name);
+
+        // 스테이지 원래 지도 출력
         for (char[] line : map) {
             System.out.println(new String(line));
+        }
+        // 데이터 기반 정보 계산
+        int width = mappedData[0].length;
+        int height = mappedData.length;
+        int holeCount = 0, ballCount = 0, playerRow = -1, playerCol = -1;
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (mappedData[i][j] == 1) holeCount++;
+                if (mappedData[i][j] == 2) ballCount++;
+                if (mappedData[i][j] == 3) {
+                    playerRow = i;
+                    playerCol = j;
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("가로크기: " + width);
+        System.out.println("세로크기: " + height);
+        System.out.println("구멍의 수: " + holeCount);
+        System.out.println("공의 수: " + ballCount);
+        if (playerRow != -1 && playerCol != -1) {
+            System.out.println("플레이어 위치: " + playerRow + "행 " + playerCol + "열");
+        } else {
+            System.out.println("플레이어 위치: 없음");
         }
     }
 }
